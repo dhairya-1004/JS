f7d857d634987b5340a59ba2c3282e675cd0a5fa
dhairya-1004 <dhairya.s.gohil@nuv.ac.in>
2025-03-20 16:40:54 +0000

    done

---
+++ .devcontainer/01_basics/variable.js
@@ -0,0 +1,17 @@
+const accountId = 1004
+let accountEmail = "dhairyagohil1004@gmail.com"
+var accountPassword  = "12345"
+accountCity = "Jaipur"
+
+accountEmail = "dg@dg.com"
+accountPassword = "dhairya1004"
+accountCity = "Bengaluru"
+
+accountEmail = "dsg@dsg.com"
+accountPassword = "dhairya10"
+accountCity = "Ahmedabad"
+
+//prefer not to use var
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
